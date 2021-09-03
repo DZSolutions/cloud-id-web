c604ce3f8fca3d6ef3bb3d978adf1fe803ec9267
KenZync <kenphati@hotmail.com>
2021-09-03 12:14:23 +0700

    upload page information + blank register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,130 +2,218 @@ import React, { useState } from "react";
 import AuthService from "../services/auth.service";
 // import jwt_decode from "jwt-decode";
 import DZLOGO from "../images/dzcard.png";
-export function Login(props){
-	const [username, setUsername] = useState("1001000"); /** Thitikorn.s */
-	const [password, setPassword] = useState("1001000"); /** .5yw8smrwxcp */
-    const [loading, setLoading] = useState(false);
-    const [message, setMessage] = useState("");
+export function Login(props) {
+  const [username, setUsername] = useState("1001000"); /** Thitikorn.s */
+  const [password, setPassword] = useState("1001000"); /** .5yw8smrwxcp */
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
 
-    const onChangeUsername = (e) => {
-        const username = e.target.value;
-        setUsername(username);
-      };
-    
-      const onChangePassword = (e) => {
-        const password = e.target.value;
-        setPassword(password);
-      };
+  const onChangeUsername = (e) => {
+    const username = e.target.value;
+    setUsername(username);
+  };
 
-      const required = (value) => {
-        if (!value) {
-          return (
-            <div>
-              This field is required!
-            </div>
-          );
-        }
-      };
-            
-    const handleLogin = (e) => {
-        e.preventDefault();
+  const onChangePassword = (e) => {
+    const password = e.target.value;
+    setPassword(password);
+  };
 
-        setMessage("");
-        setLoading(true);
-        AuthService.login(username, password).then(
-            () => {
-            props.history.push("/upload");
-            window.location.reload();
-            },
-            (error) => {
-            const resMessage =
-                (error.response &&
-                error.response.data &&
-                error.response.data.message) ||
-                error.message ||
-                error.toString();
-    
-            setLoading(false);
-            setMessage(resMessage);
-            }
-        );
+  const required = (value) => {
+    if (!value) {
+      return <div>This field is required!</div>;
     }
-    return (
-		<div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                <img className="mx-auto h-20 w-auto" src={DZLOGO} alt="org_image" />
-                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                    Sign in to your account
-                </h2>
-            </div>
-            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-                <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                    <form className="space-y-6" action="#" method="POST">
-                        <div>
-                            <label htmlFor="username" className="block text-sm font-medium text-gray-700">
-                                Username
-                            </label>
-                            <div className="mt-1">
-                                <input
-                                    type="text"
-                                    value={username}
-                                    autoComplete="username"
-                                    required
-                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-rose-500 focus:border-rose-500 sm:text-sm"
-                                    onChange={onChangeUsername}
-                                    validations={[required]}
-                                />
-                            </div>
-                        </div>
+  };
 
-                        <div>
-                            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                                Password
-                            </label>
-                            <div className="mt-1">
-                                <input
-                                    type="password"
-                                    value={password}
-                                    autoComplete="current-password"
-                                    required
-                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-rose-500 focus:border-rose-500 sm:text-sm"
-                                    onChange={onChangePassword}
-                                    validations={[required]}
-                                />
-                            </div>
-                        </div>
+  const handleLogin = (e) => {
+    e.preventDefault();
 
-                        <div className="flex items-center justify-between">
-                            <div className="flex items-center">
-                                <input
-                                    id="remember_me"
-                                    type="checkbox"
-                                    className="h-4 w-4 text-rose-600 focus:ring-rose-500 border-gray-300 rounded"
-                                />
-                                <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
-                                    Remember me
-                                </label>
-                            </div>
+    setMessage("");
+    setLoading(true);
+    AuthService.login(username, password).then(
+      () => {
+        props.history.push("/upload");
+        window.location.reload();
+      },
+      (error) => {
+        const resMessage =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
 
-                            <div className="text-sm">
-                                <a href=" " className="font-medium text-rose-600 hover:text-rose-500">
-                                    Forgot your password?
-                                </a>
-                            </div>
-                        </div>
-
-                        <div>
-                            <button
-                                onClick={handleLogin}
-                                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-rose-600 hover:bg-rose-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
-                            >
-                                Sign in
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-		</div>
+        setLoading(false);
+        setMessage(resMessage);
+      }
     );
+  };
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <img className="mx-auto h-20 w-auto" src={DZLOGO} alt="org_image" />
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              {message}
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Username
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  value={username}
+                  autoComplete="username"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-rose-500 focus:border-rose-500 sm:text-sm"
+                  onChange={onChangeUsername}
+                  validations={[required]}
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1">
+                <input
+                  type="password"
+                  value={password}
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-rose-500 focus:border-rose-500 sm:text-sm"
+                  onChange={onChangePassword}
+                  validations={[required]}
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember_me"
+                  type="checkbox"
+                  className="h-4 w-4 text-rose-600 focus:ring-rose-500 border-gray-300 rounded"
+                />
+                <label
+                  htmlFor="remember_me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <a
+                  href=" "
+                  className="font-medium text-rose-600 hover:text-rose-500"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+
+            <div className="flex space-x-4 justify-center mt-3">
+              <button
+                onClick={handleLogin}
+                disabled={loading}
+                className="w-1/2 flex items-center justify-center rounded-full bg-red-600 text-white h-10 hover:bg-red-700 focus:outline-none focus:ring"
+              >
+                {loading ? (
+                  <svg
+                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    />
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 mr-1"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+                    />
+                  </svg>
+                )}
+                Login
+              </button>
+
+              <a
+                href="register"
+                className="w-1/2 flex items-center justify-center rounded-full bg-red-600 text-white h-10 hover:bg-red-700"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                  <path
+                    fillRule="evenodd"
+                    d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                Register
+              </a>
+            </div>
+            <div className="flex justify-center mt-3">
+              <a
+                href="addticket"
+                className="w-1/2 flex items-center justify-center text-xs"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                Report Issue
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,169 +1,166 @@
-import {
-    Link,
-  } from "react-router-dom";
-  import {useState, useEffect} from "react";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import templateDZ from "../images/templateDZ.jpg";
 import AuthService from "../services/auth.service";
 import axios from "axios";
 // import mergeImages from 'merge-images';
 export function Upload() {
   const currentUser = AuthService.getCurrentUser();
-  const accessToken = localStorage.getItem('accessToken');
+  const accessToken = localStorage.getItem("accessToken");
   const [post, setPost] = useState(null);
   // const [image, setImage] = useState(null);
 
   useEffect(() => {
-    axios.get("http://localhost:8000/api/v1/userlist/?profile_id="+currentUser,{
+    axios
+      .get("http://localhost:8000/api/v1/userlist/?profile_id=" + currentUser, {
         headers: {
-            Authorization: `Bearer ${accessToken}`,
-        }
-    }).then((response) => {
-      setPost(response.data);
-    })
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
   }, []);
-    
-    if (!post) return "No post!"
-    // setImage(post.results[0].photo)
 
-    // mergeImages([templateDZ,{src:templateDZ, x:41, y:123 }])
-    // .then(b64 => setImage(b64))
+  if (!post) return "No post!";
+  // setImage(post.results[0].photo)
 
+  // mergeImages([templateDZ,{src:templateDZ, x:41, y:123 }])
+  // .then(b64 => setImage(b64))
 
-    
-
-    return (
-      // <div classNameName="px-4 py-6 sm:px-0">
-      //   <div classNameName="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-      // </div>
-      <div className="relative mt-12 sm:mt-4 lg:mt-12">
-        <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
-          <div className="mt-10 -mx-4 relative lg:mt-0 lg:col-start-1">
-            <img className="absolute mx-auto" src={templateDZ} alt="" />
-            <img className="relative mx-auto" src={post.results[0].photo} alt="" />
-          </div>
-          <div className="lg:col-start-2 md:mt-4 lg:mt-0">
-            <div className="shadow overflow-hidden sm:rounded-md">
-              <div className="px-4 py-5 bg-white sm:p-6">
-                <div className="grid gap-6 grid-cols-6">
-                  <div className="col-span-6 sm:col-span-4">
-                    <p className="block text-gray-700 text-sm font-medium">
-                      Employee/Student ID
-                    </p>
-                    <input
-                      readOnly
-                      type="text"
-                      value={currentUser}
-                      className="block mt-1 w-full border-gray-300 focus:border-blue-500 rounded-md shadow-sm focus:ring-blue-500 sm:text-sm"
-                    />
-                    {console.log(post.results[0])}
-                  </div>
-                  <div className="col-span-6 sm:col-span-3">
-                    <p className="block text-gray-700 text-sm font-medium">
-                      First name
-                    </p>
-                    <input
-                      readOnly
-                      type="text"
-                      value={post.results[0].first_name_en}
-                      className="block mt-1 w-full border-gray-300 focus:border-blue-500 rounded-md shadow-sm focus:ring-blue-500 sm:text-sm"
-                    />
-                  </div>
-  
-                  <div className="col-span-6 sm:col-span-3">
-                    <p className="block text-gray-700 text-sm font-medium">
-                      Last name
-                    </p>
-                    <input
-                      readOnly
-                      type="text"
-                      value={post.results[0].last_name_en}
-                      className="block mt-1 w-full border-gray-300 focus:border-blue-500 rounded-md shadow-sm focus:ring-blue-500 sm:text-sm"
-                    />
-                  </div>
-  
-                  <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                    <p className="block text-gray-700 text-sm font-medium">
-                      Mobile No.
-                    </p>
-                    <input
-                      readOnly
-                      type="text"
-                      value={post.results[0].phone}
-                      className="block mt-1 w-full border-gray-300 focus:border-blue-500 rounded-md shadow-sm focus:ring-blue-500 sm:text-sm"
-                    />
-                  </div>
-  
-                  <div className="col-span-6 sm:col-span-4">
-                    <p className="block text-gray-700 text-sm font-medium">
-                      Email address
-                    </p>
-                    <input
-                      readOnly
-                      type="text"
-                      className="block mt-1 w-full border-gray-300 focus:border-blue-500 rounded-md shadow-sm focus:ring-blue-500 sm:text-sm"
-                    />
-                  </div>
-  
-                  <div className="col-span-6">
-                    <p className="block text-gray-700 text-sm font-medium">
-                      Department / Faculty
-                    </p>
-                    <input
-                      readOnly
-                      type="text"
-                      value={post.results[0].faculty}
-                      className="block mt-1 w-full border-gray-300 focus:border-blue-500 rounded-md shadow-sm focus:ring-blue-500 sm:text-sm"
-                    />
-                  </div>
-  
-                  <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                    <p className="block text-gray-700 text-sm font-medium">
-                      Issue Date
-                    </p>
-                    <input
-                      readOnly
-                      type="text"
-                      value={post.results[0].issue_date}
-                      className="block mt-1 w-full border-gray-300 focus:border-blue-500 rounded-md shadow-sm focus:ring-blue-500 sm:text-sm"
-                    />
-                  </div>
-  
-                  <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                    <p className="block text-gray-700 text-sm font-medium">
-                      Expire Date
-                    </p>
-                    <input
-                      readOnly
-                      type="text"
-                      value={post.results[0].expire_date}
-                      className="block mt-1 w-full border-gray-300 focus:border-blue-500 rounded-md shadow-sm focus:ring-blue-500 sm:text-sm"
-                    />
-                  </div>
-                  <div className="col-span-6"></div>
-                </div>
-              </div>
-              <div className="px-4 py-3 text-center bg-gray-100 sm:px-6">
-                <div className="flex items-center justify-center col-span-2">
+  return (
+    <div className="relative mt-12 sm:mt-4 lg:mt-12">
+      <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
+        <div className="mt-10 -mx-4 relative lg:mt-0 lg:col-start-1">
+          <img className="absolute mx-auto" src={templateDZ} alt="" />
+          <img
+            className="relative mx-auto"
+            src={post.results[0].photo}
+            alt=""
+          />
+        </div>
+        <div className="lg:col-start-2 md:mt-4 lg:mt-0">
+          <div className="shadow overflow-hidden sm:rounded-md">
+            <div className="px-4 py-5 bg-white sm:p-6">
+              <div className="grid gap-6 grid-cols-6">
+                <div className="col-span-6 sm:col-span-4">
+                  <p className="block text-gray-700 text-sm font-medium">
+                    Employee/Student ID
+                  </p>
                   <input
-                    id="accept_tos"
-                    name="accept_tos"
-                    type="checkbox"
-                    className="h-8 w-8 text-indigo-600 focus:ring-indigo-500 border-gray-700 rounded"
-                  ></input>
-                  <label className="ml-2 block text-sm font-medium text-gray-700">
-                    I agree to the terms and conditions and the privacy policy
-                  </label>
+                    readOnly
+                    type="text"
+                    value={currentUser}
+                    className="block mt-1 w-full border-gray-300 focus:border-blue-500 rounded-md shadow-sm focus:ring-blue-500 sm:text-sm"
+                  />
                 </div>
-                <Link
-                  to="/CropImage"
-                  className="inline-flex justify-center px-4 py-2 text-white text-3xl font-medium bg-rose-600 hover:bg-rose-500 border border-transparent rounded-full focus:outline-none shadow-sm focus:ring-rose-500 focus:ring-offset-2 focus:ring-2"
-                >
-                  Upload Photo
-                </Link>
+                <div className="col-span-6 sm:col-span-3">
+                  <p className="block text-gray-700 text-sm font-medium">
+                    First name
+                  </p>
+                  <input
+                    readOnly
+                    type="text"
+                    value={post.results[0].first_name_en}
+                    className="block mt-1 w-full border-gray-300 focus:border-blue-500 rounded-md shadow-sm focus:ring-blue-500 sm:text-sm"
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-3">
+                  <p className="block text-gray-700 text-sm font-medium">
+                    Last name
+                  </p>
+                  <input
+                    readOnly
+                    type="text"
+                    value={post.results[0].last_name_en}
+                    className="block mt-1 w-full border-gray-300 focus:border-blue-500 rounded-md shadow-sm focus:ring-blue-500 sm:text-sm"
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                  <p className="block text-gray-700 text-sm font-medium">
+                    Mobile No.
+                  </p>
+                  <input
+                    readOnly
+                    type="text"
+                    value={post.results[0].phone}
+                    className="block mt-1 w-full border-gray-300 focus:border-blue-500 rounded-md shadow-sm focus:ring-blue-500 sm:text-sm"
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-4">
+                  <p className="block text-gray-700 text-sm font-medium">
+                    Email address
+                  </p>
+                  <input
+                    readOnly
+                    type="text"
+                    className="block mt-1 w-full border-gray-300 focus:border-blue-500 rounded-md shadow-sm focus:ring-blue-500 sm:text-sm"
+                  />
+                </div>
+
+                <div className="col-span-6">
+                  <p className="block text-gray-700 text-sm font-medium">
+                    Department / Faculty
+                  </p>
+                  <input
+                    readOnly
+                    type="text"
+                    value={post.results[0].faculty}
+                    className="block mt-1 w-full border-gray-300 focus:border-blue-500 rounded-md shadow-sm focus:ring-blue-500 sm:text-sm"
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                  <p className="block text-gray-700 text-sm font-medium">
+                    Issue Date
+                  </p>
+                  <input
+                    readOnly
+                    type="text"
+                    value={post.results[0].issue_date}
+                    className="block mt-1 w-full border-gray-300 focus:border-blue-500 rounded-md shadow-sm focus:ring-blue-500 sm:text-sm"
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                  <p className="block text-gray-700 text-sm font-medium">
+                    Expire Date
+                  </p>
+                  <input
+                    readOnly
+                    type="text"
+                    value={post.results[0].expire_date}
+                    className="block mt-1 w-full border-gray-300 focus:border-blue-500 rounded-md shadow-sm focus:ring-blue-500 sm:text-sm"
+                  />
+                </div>
+                <div className="col-span-6"></div>
               </div>
+            </div>
+            <div className="px-4 py-3 text-center bg-gray-100 sm:px-6">
+              <div className="flex items-center justify-center col-span-2">
+                <input
+                  id="accept_tos"
+                  name="accept_tos"
+                  type="checkbox"
+                  className="h-8 w-8 text-indigo-600 focus:ring-indigo-500 border-gray-700 rounded"
+                ></input>
+                <label className="ml-2 block text-sm font-medium text-gray-700">
+                  I agree to the terms and conditions and the privacy policy
+                </label>
+              </div>
+              <Link
+                to="/CropImage"
+                className="inline-flex justify-center px-4 py-2 text-white text-3xl font-medium bg-rose-600 hover:bg-rose-500 border border-transparent rounded-full focus:outline-none shadow-sm focus:ring-rose-500 focus:ring-offset-2 focus:ring-2"
+              >
+                Upload Photo
+              </Link>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
