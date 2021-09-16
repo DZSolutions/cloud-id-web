26c9b3a01cdfeba1052be7ac52bac5b6fbcb2d00
KenZync <kenphati@hotmail.com>
2021-09-16 16:59:44 +0700

    Add Remove Background Function

--- src/pages/ImageCropper.js
+++ src/pages/ImageCropper.js
@@ -1,6 +1,10 @@
-import { useRef, useState } from "react";
+import { useRef, useState, Fragment } from "react";
 import Cropper from "react-easy-crop";
-import { generateDownload } from "../utils/cropImage";
+import getCroppedImg, { generateDownload } from "../utils/cropImage";
+import { Dialog, Transition } from "@headlessui/react";
+import { CheckIcon } from "@heroicons/react/outline";
+import { API_BASE_URL } from "../constrants/apiConstrants";
+import axios from "axios";
 export function ImageCropper() {
   const inputRef = useRef();
 
@@ -10,6 +14,11 @@ export function ImageCropper() {
   const [croppedArea, setCroppedArea] = useState(null);
   const [crop, setCrop] = useState({ x: 0, y: 0 });
   const [zoom, setZoom] = useState(1);
+
+  const [uploaded, setUploaded] = useState([]);
+  const cancelButtonRef = useRef(null);
+  const [open, setOpen] = useState(false);
+  const [isUploading, setIsUpLoading] = useState(false);
 
   const onCropComplete = (croppedAreaPercentage, croppedAreaPixels) => {
     setCroppedArea(croppedAreaPixels);
@@ -28,59 +37,207 @@ export function ImageCropper() {
   const onDownload = () => {
     generateDownload(image, croppedArea);
   };
-  return (
-    <div className="bg-gray-700 px-4 py-5 rounded-md">
-      <div className="bg-black px-4 py-64 rounded-md relative">
-        {image ? (
-          <>
-            <Cropper
-              image={image}
-              crop={crop}
-              zoom={zoom}
-              aspect={1}
-              onCropChange={setCrop}
-              onZoomChange={setZoom}
-              onCropComplete={onCropComplete}
-            />
-          </>
-        ) : null}
-      </div>
-      {image ? (
-        <div className="flex justify-center pt-5">
-          <input
-            type="range"
-            min="1"
-            step="0.1"
-            max="3"
-            value={zoom}
-            onChange={(e) => setZoom(e.target.value)}
-          ></input>
-        </div>
-      ) : null}
 
-      <div className="container-buttons flex justify-center space-x-4 pt-5">
-        <input
-          className="hidden"
-          type="file"
-          accept="image/*"
-          ref={inputRef}
-          onChange={onSelectFile}
-        ></input>
-        <button
-          type="button"
-          className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          onClick={triggerFileSelectPopup}
-        >
-          Choose
-        </button>
-        <button
-          type="button"
-          className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          onClick={onDownload}
-        >
-          Download
-        </button>
+  const onUpload = async () => {
+    setIsUpLoading(true);
+    const canvas = await getCroppedImg(image, croppedArea);
+    const base64Canvas = canvas.toDataURL("image/jpeg").split(";base64,")[1];
+
+    let config = {
+      headers: {
+        "X-Client-Secret": "DZSolution_Secret_Client",
+        "Content-Type": "application/json",
+      },
+    };
+
+    var payload = JSON.stringify({
+      image: base64Canvas,
+      fill_color: { red: 174, green: 126, blue: 186 },
+    });
+
+    const response = await axios.post(
+      "https://api.dzcardsolutions.com/api/v1/image/background/remove",
+      payload,
+      config
+    );
+    setUploaded(response.data);
+    setOpen(true);
+    setIsUpLoading(false);
+  };
+
+  return (
+    <>
+      <div className="bg-gray-700 px-4 py-5 rounded-md">
+        <div className="bg-black px-4 py-64 rounded-md relative">
+          {image ? (
+            <>
+              <Cropper
+                image={image}
+                crop={crop}
+                zoom={zoom}
+                aspect={1}
+                onCropChange={setCrop}
+                onZoomChange={setZoom}
+                onCropComplete={onCropComplete}
+              />
+            </>
+          ) : null}
+        </div>
+        {image ? (
+          <div className="flex justify-center pt-5">
+            <input
+              type="range"
+              min="1"
+              step="0.1"
+              max="3"
+              value={zoom}
+              onChange={(e) => setZoom(e.target.value)}
+            ></input>
+          </div>
+        ) : null}
+
+        <div className="container-buttons flex justify-center space-x-4 pt-5">
+          <input
+            className="hidden"
+            type="file"
+            accept="image/*"
+            ref={inputRef}
+            onChange={onSelectFile}
+          ></input>
+          <button
+            type="button"
+            className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={triggerFileSelectPopup}
+          >
+            Choose
+          </button>
+          {image ? (
+            <>
+              <button
+                type="button"
+                className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                onClick={onUpload}
+                disabled={isUploading}
+              >
+                {isUploading ? (
+                  <svg
+                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    />
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    />
+                  </svg>
+                ) : null}
+                Upload
+              </button>
+              <button
+                type="button"
+                className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                onClick={onDownload}
+              >
+                Download
+              </button>
+            </>
+          ) : null}
+        </div>
       </div>
-    </div>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed z-10 inset-0 overflow-y-auto"
+          initialFocus={cancelButtonRef}
+          onClose={setOpen}
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                <div>
+                  <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+                    <CheckIcon
+                      className="h-6 w-6 text-green-600"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg leading-6 font-medium text-gray-900"
+                    >
+                      Remove Background Successful
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <img
+                        className="mx-auto w-auto"
+                        src={`data:image/jpeg;base64,${uploaded.image}`}
+                        alt="org_image"
+                      />
+                      <p className="text-sm pt-2 text-gray-500">
+                        Do you want to upload this picture?
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
+                  <button
+                    type="button"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
+                    onClick={() => setOpen(false)}
+                  >
+                    Upload
+                  </button>
+                  <button
+                    type="button"
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm"
+                    onClick={() => setOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </>
   );
 }
