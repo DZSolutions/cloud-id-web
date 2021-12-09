65c5cc766e4094217892e2a6d8152c21729d7ee2
!SharkPongsatorn <33623726+l3oaShark@users.noreply.github.com>
2021-12-09 13:32:37 +0700

    enable choose button

--- src/pages/ImageCropper.js
+++ src/pages/ImageCropper.js
@@ -465,7 +465,7 @@ export function ImageCropper(props) {
             </button>
             )
           }
-          {/* {ischoosephoto && (
+          {ischoosephoto && (
             <button
               type="button"
               className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
@@ -477,7 +477,7 @@ export function ImageCropper(props) {
               Choose
             </button>
 
-          )} */}
+          )}
 
           {/* <button
                 type="button"
