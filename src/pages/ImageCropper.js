127a6b2fed374eed8f2866c413201930b8c25a26
!SharkPongsatorn <33623726+l3oaShark@users.noreply.github.com>
2021-12-09 13:58:49 +0700

    set can't click back drop

--- src/pages/ImageCropper.js
+++ src/pages/ImageCropper.js
@@ -555,7 +555,9 @@ export function ImageCropper(props) {
           as="div"
           className="fixed z-10 inset-0 overflow-y-auto"
           // initialFocus={cancelButtonRef}
-          onClose={setOpen}
+          onClose={(event, reason) => {
+            handleClose(event, reason);
+        }}
         >
           <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
             <Transition.Child
