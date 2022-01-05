00730808b3c79eedbc1df81d1d4f90bce9fbf9ad
!SharkPongsatorn <33623726+l3oaShark@users.noreply.github.com>
2022-01-05 12:41:15 +0700

    add api get card and gencard

--- src/constrants/apiConstrants.js
+++ src/constrants/apiConstrants.js
@@ -1,3 +1,5 @@
 // export const API_BASE_URL = process.env.REACT_APP_SERVER_URL;
 // export const API_BASE_URL = "http://localhost:8000/api";
 export const API_BASE_URL = "https://photodev.dzcardsolutions.com/api";
+export const API_GENCARD_IMG_URL = "http://13.212.202.194:8033/gen_card_img/";
+export const API_REQUEST_CARDTEMPLATE_URL = "http://13.212.202.194:8033/card_design_img/";
