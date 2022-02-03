313d36514c1e487549a27e7288bc3928c00d726a
!SharkPongsatorn <33623726+l3oaShark@users.noreply.github.com>
2022-02-03 10:53:56 +0700

    add api get size crop img

--- src/constrants/apiConstrants.js
+++ src/constrants/apiConstrants.js
@@ -2,5 +2,6 @@
 // export const API_BASE_URL = "http://localhost:8000/api";
 export const API_BASE_URL = "https://photodev.dzcardsolutions.com/api";
 export const API_GENCARD_IMG_URL = "https://api-carddesign.dzcardsolutions.com/gen_card_img/";
+export const API_GET_IMG_SIZE_URL = "https://api-carddesign.dzcardsolutions.com/card_design_img_size/";
 export const API_REQUEST_CARDTEMPLATE_URL = "https://api-carddesign.dzcardsolutions.com/card_design_img/";
 export const API_REQUEST_PRINTER_URL = "https://cloudprint.dzcardsolutions.com/api/printers/";
