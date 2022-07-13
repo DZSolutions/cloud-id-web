260561d0bfbf708f12cb16d00ebb4d5f8e58102e
!SharkPongsatorn <33623726+l3oaShark@users.noreply.github.com>
2022-07-13 22:33:51 +0700

    add field register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,9 +4,55 @@ import dzLogo from "../images/dzcard.png";
 import { API_BASE_URL } from "../constrants/apiConstrants";
 import { Link } from "react-router-dom";
 import swal from 'sweetalert';
+
+import React, { Fragment } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+
+
+// const useStyles = makeStyles(theme => ({
+//   logo: {
+//       display: 'flex',
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//       padding: '0.5rem',
+//       color: '#745c97',
+//       fontWeight: 'bold',
+//       letterSpacing: '1rem'
+//   },
+//   border: {
+//       border: '0.15rem solid #d597ce',
+//       borderRadius: '2px',
+//       padding: '2%',
+//       width: '19rem',
+//       textAlign: 'center'
+//   },
+//   topLayout: {
+//       margin: '4rem 0',
+//       [theme.breakpoints.down('xs')]: {
+//           margin: '1rem 0'
+//       }
+//   },
+//   paperLayout: {
+//       padding: '2rem',
+//       [theme.breakpoints.up('md')]: {
+//           width: '35em'
+//       },
+//       marginTop: '10rem',
+//       margin: 'auto',
+//       border: '1px solid #ebedf0',
+//       borderRadius: '4px',
+//       [theme.breakpoints.down('xs')]: {
+//           marginTop: '3rem'
+//       }
+//   },
+// }));
+
 export function Register(props) {
   const [organizationList, setOrganizationList] = useState([]);
   const [logo, setLogo] = useState(null);
+
 
   // const onChangeOrganization = (e) => {
   //   const organization = e.target.value;
@@ -56,6 +102,14 @@ export function Register(props) {
     faculty: "-",
     major: "-",
     phone: "",
+    birthDate :"",
+    gender :0,
+    facebook :"",
+    lineID :"",
+    linkedin :"",
+    address:"",
+    other :"",
+    details:""
   });
 
   const handleChange = (e) => {
@@ -126,6 +180,14 @@ export function Register(props) {
         faculty: "-",
         major: "-",
         phone: state.phone,
+        birthDate :state.birthDate,
+        gender :state.gender,
+        facebook :state.facebook,
+        lineID :state.lineID,
+        linkedin :state.linkedin,
+        address:state.address,
+        other :state.other,
+        details:state.details
       })
       .then((response) => {
         if (response.status === 200) {
@@ -152,7 +214,36 @@ export function Register(props) {
   };
 
   return (
+
+
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-4 sm:px-6 lg:px-8">
+
+{/* <Fragment>
+            <Grid container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                className={classes.topLayout}
+            >
+                <Grid item md={11} xs={11}>
+                    <Paper className={classes.paperLayout}>
+                        <Grid container>
+                            <Grid item md={12} xs={12}>
+                                <div className={classes.logo}>
+                                    <div className={classes.border}>
+                                        REGISTER
+                                    </div>
+                                </div>
+                            </Grid>
+                            <Grid item md={12} xs={12}>
+                                <StepperForm/>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Fragment> */}
+
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         {/* <img
           src={dzLogo}
@@ -331,6 +422,52 @@ export function Register(props) {
                 />
               </div>
             </div>
+
+            <div>
+              <label
+                htmlFor="tel"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Birth Date
+              </label>
+              <div className="mt-1">
+                <input
+                  value={state.birthDate}
+                  onChange={handleChange}
+                  id="birthDate"
+                  name="birthDate"
+                  type="date"
+                  autoComplete="off"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="sex"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Gender
+              </label>
+              <div className="mt-1">
+
+                <select
+                  value={state.gender}
+                  onChange={handleChange}
+                  id="gender"
+                  name="gender"
+                  className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                >
+                  <option defaultValue value="0">Male</option>
+                  <option value="1">Female</option>
+                  <option value="2">Other</option>
+
+                </select>
+              </div>
+            </div>
+
             {/* <div className="grid grid-cols-6 gap-6">
               <div className="col-span-6 sm:col-span-3">
                 <label
@@ -391,6 +528,127 @@ export function Register(props) {
                 />
               </div>
             </div>
+
+            <div>
+              <label
+                htmlFor="tel"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Facebook
+              </label>
+              <div className="mt-1">
+                <input
+                  value={state.facebook}
+                  onChange={handleChange}
+                  id="facebook"
+                  name="facebook"
+                  type="text"
+                  autoComplete="off"
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="tel"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Line ID
+              </label>
+              <div className="mt-1">
+                <input
+                  value={state.lineID}
+                  onChange={handleChange}
+                  id="lineID"
+                  name="lineID"
+                  type="text"
+                  autoComplete="off"
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="tel"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Linkedin
+              </label>
+              <div className="mt-1">
+                <input
+                  value={state.linkedin}
+                  onChange={handleChange}
+                  id="linkedin"
+                  name="linkedin"
+                  type="text"
+                  autoComplete="off"
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="tel"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Address
+              </label>
+              <div className="mt-1">
+                <input
+                 value={state.address}
+                  onChange={handleChange}
+                  id="address"
+                  name="address"
+                  type="text"
+                  autoComplete="address-line1"
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="tel"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Other
+              </label>
+              <div className="mt-1">
+                <input
+                  value={state.other}
+                  onChange={handleChange}
+                  id="other"
+                  name="other"
+                  type="text"
+                  autoComplete="off"
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="tel"
+                className="block text-sm font-medium text-gray-700"
+              >
+                details
+              </label>
+              <div className="mt-1">
+                <input
+                  value={state.details}
+                  onChange={handleChange}
+                  id="details"
+                  name="details"
+                  type="text"
+                  autoComplete="off"
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+
             <div className="flex space-x-2">
               <Link
                 to={"/" + props.match.params.org}
