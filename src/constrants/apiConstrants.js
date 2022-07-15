732039ee0b77eac40f247172f7c1c146d0a7f974
!SharkPongsatorn <33623726+l3oaShark@users.noreply.github.com>
2022-07-15 15:13:22 +0700

    add url remove bg

--- src/constrants/apiConstrants.js
+++ src/constrants/apiConstrants.js
@@ -5,3 +5,4 @@ export const API_GENCARD_IMG_URL = "https://api-carddesign.dzcardsolutions.com/g
 export const API_GET_IMG_SIZE_URL = "https://api-carddesign.dzcardsolutions.com/card_design_img_size/";
 export const API_REQUEST_CARDTEMPLATE_URL = "https://api-carddesign.dzcardsolutions.com/card_design_img/";
 export const API_REQUEST_PRINTER_URL = "https://cloudprint.dzcardsolutions.com/api/printers/";
+export const API_REMOVE_BG = "https://api.dzcardsolutions.com/api/v1/image/background/remove";
