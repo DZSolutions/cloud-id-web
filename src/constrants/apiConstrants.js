64399422e0e2e2271ddd0f7f380e182f00b6d2e5
!SharkPongsatorn <33623726+l3oaShark@users.noreply.github.com>
2022-07-18 16:27:15 +0700

    add api print

--- src/constrants/apiConstrants.js
+++ src/constrants/apiConstrants.js
@@ -6,3 +6,4 @@ export const API_GET_IMG_SIZE_URL = "https://api-carddesign.dzcardsolutions.com/
 export const API_REQUEST_CARDTEMPLATE_URL = "https://api-carddesign.dzcardsolutions.com/card_design_img/";
 export const API_REQUEST_PRINTER_URL = "https://cloudprint.dzcardsolutions.com/api/printers/";
 export const API_REMOVE_BG = "https://api.dzcardsolutions.com/api/v1/image/background/remove";
+export const API_POST_PRINT_URL ="https://cloudprint.dzcardsolutions.com/api/jobs/";
