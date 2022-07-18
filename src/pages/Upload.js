42afb00f5d368ed8e4a725add256b6488468ae0e
!SharkPongsatorn <33623726+l3oaShark@users.noreply.github.com>
2022-07-18 14:51:13 +0700

    add url print job

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -5,7 +5,7 @@ import templateDZ_Back from "../images/templateDZ_Back.jpg";
 import AuthService from "../services/auth.service";
 import axios from "axios";
 import mergeImages from "merge-images";
-import { API_BASE_URL,API_GENCARD_IMG_URL,API_REQUEST_PRINTER_URL } from "../constrants/apiConstrants";
+import { API_BASE_URL,API_GENCARD_IMG_URL,API_REQUEST_PRINTER_URL,API_POST_PRINT_URL } from "../constrants/apiConstrants";
 import Select, { components } from "react-select";
 import { Dialog, Transition } from "@headlessui/react";
 import { CheckIcon,CreditCardIcon,IdentificationIcon,QrcodeIcon,RewindIcon } from "@heroicons/react/outline";
@@ -1273,7 +1273,7 @@ export function Upload(props) {
      }
 
       await axios
-         .post(API_REQUEST_PRINTER_URL, {
+         .post(API_POST_PRINT_URL, {
            console: configPrinter.results[0].consoleID,
            printer: configPrinter.results[0].printer_ID,
            print_option:printerOption,
